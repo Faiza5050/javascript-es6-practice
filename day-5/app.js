f599763f142e2a1ca79d1efadd37665ddef49283
Faiza5050 <faizakanwal5050@gmail.com>
2025-01-31 02:08:14 +0500

    es6 practice

---
+++ day-5/app.js
@@ -0,0 +1,17 @@
+// export default function Mod() {
+//     return "Hello Faiza";
+// }
+
+// export function Mod2() {
+//     console.log("Hello Model 2");
+// }
+
+// // Module With Variables:
+// export const nameMod = "Maha";
+
+// // Module With Class:
+// export class ModClass {
+//     myClass() {
+//         console.log("Hello Class");
+//     }
+// }
